425ca2002a56dc9c49d0cdf713546ab10d73d6b9
Turbojax07 <turbojax07@gmail.com>
2023-12-06 02:43:17 -0500

    Update Constants.java

--- src/main/java/hue/control/Constants.java
+++ src/main/java/hue/control/Constants.java
@@ -3,8 +3,8 @@ package hue.control;
 import java.awt.Color;
 
 public class Constants {
-    public static final String ip = "10.180.51.60";
-    public static final String username = "oSpVxc0qDnFRlPKDIkMsPLWAQdE9Y38oa91AOBCg";
+    public static final String ip = "bridgeIP";
+    public static final String username = "yourUsernameHere";
     public static final String baseUrl = "http://" + ip + "/api/" + username + "/lights/";
     public static final boolean defaultState = false;
     public static final Color defaultColor = Color.white;
